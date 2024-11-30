a03ac7a68cfbcddcea729115fa06977219330c93
Mchedlishvili-Ilia <iliali12@my.yorku.ca>
2024-11-30 00:06:40 -0500

    Destructuring the courses and name. Adding test scores array and logging

--- script.mjs
+++ script.mjs
@@ -6,12 +6,23 @@ const student = {
   courses: ["Math", "Science", "Art"],
   
 
+
   displayInfo: function() {
       return `Student Name: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled ? 'Yes' : 'No'}`;
   }
 };
 
- 
+const { name, courses } = student;
+console.log("Name:", name);
+console.log("Courses:", courses);
+
+const scores = [85, 92, 78, 90];
+
+const [firstScore, secondScore] = scores;
+console.log("First Score:", firstScore);
+console.log("Second Score:", secondScore);
+
+
 const studentJSON = JSON.stringify(student);
 console.log("JSON String:", studentJSON);
 
